81a62467de9dc21417453d21d775f558f4f5c320
KacperKropacz <89976303+KacperKropacz@users.noreply.github.com>
2022-03-31 12:53:29 +0200

    Add files via upload

---
+++ bundestag.py
@@ -0,0 +1,18 @@
+'''
+import matplotlib.pyplot as plt
+import numpy as np
+
+y = np.array([35, 25, 25, 15])
+
+plt.pie(y)
+plt.show() 
+'''
+import matplotlib.pyplot as plt
+import numpy as np
+
+y = np.array([28, 26, 16, 12, 11, 5, 2])
+mylabels = ["SPD", "CDU/CSU", "Greens", "FDP", "AFD", "Left Party", "Others"]
+myexplode = [0, 0, 0, 0, 0, 0, 0]
+
+plt.pie(y, labels = mylabels, explode = myexplode, autopct = '%.0f%%')
+plt.show() 
